88f516515f819cf59db2499ee0969528242cb934
terezshashikyan <78693438+terezshashikyan@users.noreply.github.com>
2023-08-26 16:51:33 +0400

    style changes

--- src/Components/Todos/index.jsx
+++ src/Components/Todos/index.jsx
@@ -3,14 +3,10 @@ import { TodoItem } from "../TodoItem";
 import { useDispatch, useSelector } from "react-redux";
 import { userOp, userSel } from "../../store/user";
 import { auth } from "../../firebase";
-import {  onAuthStateChanged} from "firebase/auth";
 import './styles.css';
-import { db } from "../../firebase";
-import { doc, getDoc, getDocs, collection, updateDoc, arrayUnion, arrayRemove } from "firebase/firestore";
+
 
 export const Todos = () => {
-
- 
   const dispatch = useDispatch();
   const todosList = useSelector(userSel.todosListSelector);
   const [todoText, setTodoText] = useState('');
@@ -37,15 +33,7 @@ export const Todos = () => {
     }
   };
 
-  const changeTodoStatus = (td) => {
-    dispatch(
-      userOp.updateTodo(auth.currentUser.uid, td)
-    )
-  }
-
   useEffect(() => {
-  getDocs(collection(db, "users",'userId', 'todosList')).then ((result) => console.log(result.docs))
-    console.log(auth.currentUser.uid)
      dispatch(userOp.getTodosList(auth.currentUser.uid));
    }, [dispatch]);
   
@@ -54,14 +42,16 @@ export const Todos = () => {
   return    (
     
   <div className = 'todos-section'>
+
     {/* input section for adding new todos*/}
+    <h1>Todos</h1>
+    <h2>Organize your work and life, finally.</h2>
     <div className="addtodo-section">
     <input value={todoText} placeholder = "Add more todos!" onChange={(e) => setTodoText(e.target.value)} />
       <button onClick={() => createTodo()}>Add</button>
     </div>
     
-    {/* TodosList section: First, we should check if there are any todos and if not showcase the message that there are not any todos yet. */}
-    
+    {/* TodosList section: First, we should check if there are any todos.*/}
     <div className="todos-list">    
       {todosList?.map((todo) => (
         <div key = {todo.todoText}>
@@ -69,6 +59,7 @@ export const Todos = () => {
         </div>
       ))}
     </div> 
+
   </div>
    )
 };
