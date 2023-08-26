02009ee395fd1f8120fec5fa1626240f3c78b5e7
terezshashikyan <78693438+terezshashikyan@users.noreply.github.com>
2023-08-26 16:19:13 +0400

    style changes

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -1,11 +1,18 @@
 import { Link } from "react-router-dom";
+import plant from './plant.png';
 import './styles.css';
 
 export const Home = () => {
   return <>
   <section className="homepage-section">
+  <div className="text">
   <h1>Todos App</h1>
-  <h2>JSON Placeholder: Free fake API for testing and prototyping. Powered by JSON Server + LowDB. Tested with XV. </h2>
+  <h2>Become focused, organized, and calm with Todos App. The world’s #1 task manager and to-do list app.</h2>
+  <button><Link to={"/todos"}><h2>Get started!</h2></Link></button>
+  </div>
+  <div className="image">
+  <img src={plant} alt="homePage" />
+  </div>
   </section>
   </>
 };
